3a71bb2e433345646e83123acdc1b662265dad96
BSvyatoslav <sbugrov@isgneuro.com>
2023-07-20 13:32:02 +0300

    moved output capturing into a def

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,4 +1,3 @@
-import re
 import subprocess
 from unittest import TestCase
 from pandas import DataFrame
@@ -8,8 +7,12 @@ TEST_STRING = "| readFile example_002.csv type=csv storage=pp_storage | tail 5"
 EXPECTED_RESULT = "readFile example_002.csv type=csv storage=pp_storage"
 
 
-def get_df():
-    print('a')
+def capture(s):
+    x = subprocess.run(['pp', s], capture_output=True).stdout.decode()
+    x = [n.split() for n in x[:-1].split('\n')]
+    x = [x[0]] + [lst[1:] for lst in x[1:]]
+    x = DataFrame(x[1:], columns=x[0])
+    return x
 
 
 class TestCommand(TestCase):
@@ -17,15 +20,10 @@ class TestCommand(TestCase):
     # нужно протестировать
     # n
     def test_n(self):
-        sample = subprocess.run(['pp', EXPECTED_RESULT], capture_output=True).stdout.decode()
-        sample = [x.split() for x in sample[:-1].split('\n')]
-        sample = [sample[0]]+[lst[1:] for lst in sample[1:]]
-        result = subprocess.run(['pp', TEST_STRING], capture_output=True).stdout.decode()
-        result = [x.split() for x in result[:-1].split('\n')]
-        result = [result[0]] + [lst[1:] for lst in result[1:]]
-        df = DataFrame(result[1:], columns=result[0])
+        sample = capture(EXPECTED_RESULT)
+        result = capture(TEST_STRING)
 
-        self.assertEqual(sample, df)
+        self.assertEqual(sample, result)
     #
     #
     #
