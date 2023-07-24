a6f7c403aba6d8504a7bcd50303b16381bea52b3
BSvyatoslav <sbugrov@isgneuro.com>
2023-07-24 13:39:00 +0300

    formated tests according to pylint

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,23 +1,33 @@
+"""Module for testing tail command"""
 import subprocess
 from unittest import TestCase
 
-SAMPLE = [['aa', 'b', 'c'], ['3', 'AAA', '10001'], ['3', 'DDD', '10002'], ['1', 'EEE', '1000001'], ['4', 'CCC', '3'],
-          ['3', 'BBB', '15']]
+SAMPLE = [["aa", "b", "c"],
+          ["3", "AAA", "10001"],
+          ["3", "DDD", "10002"],
+          ["1", "EEE", "1000001"],
+          ["4", "CCC", "3"],
+          ["3", "BBB", "15"]]
 
 
 def capture(s):
-    """Captures the output of postprocessing command s and turns it into a list"""
-    x = subprocess.run(['pp', s], capture_output=True).stdout.decode()
-    x = [n.split() for n in x[:-1].split('\n')]
+    """Captures the output of postprocessing
+     command s and turns it into a list"""
+    x = subprocess.run(["pp", s],
+                       check=False,
+                       capture_output=True).stdout.decode()
+    x = [n.split() for n in x[:-1].split("\n")]
     x = [x[0]] + [lst[1:] for lst in x[1:]]
     return x
 
 
 class TestCommand(TestCase):
+    """Class for testing tail command"""
     # Testing N
     def test_n(self):
         for i in range(2, 6):
-            test_string = f"| readFile example_002.csv type=csv storage=pp_storage | tail {i}"
+            test_string = f"| readFile example_002.csv type=csv " \
+                          f"storage=pp_storage | tail {i}"
             sample = [SAMPLE[0]] + SAMPLE[len(SAMPLE) - i:]
             result = capture(test_string)
             self.assertEqual(sample, result)
@@ -25,7 +35,8 @@ class TestCommand(TestCase):
     # Testing limit
     def test_limit(self):
         for i in range(2, 6):
-            test_string = f"| readFile example_002.csv type=csv storage=pp_storage | tail limit={i}"
+            test_string = f"| readFile example_002.csv type=csv" \
+                          f" storage=pp_storage | tail limit={i}"
             sample = [SAMPLE[0]] + SAMPLE[len(SAMPLE) - i:]
             result = capture(test_string)
             self.assertEqual(sample, result)
@@ -34,7 +45,8 @@ class TestCommand(TestCase):
     def test_both(self):
         for i in range(2, 6):
             for j in range(2, 6):
-                test_string = f"| readFile example_002.csv type=csv storage=pp_storage | tail {i} limit={j}"
+                test_string = f"| readFile example_002.csv type=csv" \
+                              f" storage=pp_storage | tail {i} limit={j}"
                 sample = [SAMPLE[0]] + SAMPLE[len(SAMPLE) - i:]
                 result = capture(test_string)
                 self.assertEqual(sample, result)
@@ -42,7 +54,8 @@ class TestCommand(TestCase):
     # all the same tests, but compared with wrong answers
     def test_bad_n(self):
         for i in range(2, 6):
-            test_string = f"| readFile example_002.csv type=csv storage=pp_storage | tail {i}"
+            test_string = f"| readFile example_002.csv type=csv " \
+                          f"storage=pp_storage | tail {i}"
             sample = [SAMPLE[0]] + SAMPLE[len(SAMPLE) - i + 1:]
             sample1 = [SAMPLE[0]] + SAMPLE[len(SAMPLE) - i - 1:]
             result = capture(test_string)
@@ -51,7 +64,8 @@ class TestCommand(TestCase):
 
     def test_bad_l(self):
         for i in range(2, 6):
-            test_string = f"| readFile example_002.csv type=csv storage=pp_storage | tail limit={i}"
+            test_string = f"| readFile example_002.csv type=csv " \
+                          f"storage=pp_storage | tail limit={i}"
             sample = [SAMPLE[0]] + SAMPLE[len(SAMPLE) - i + 1:]
             sample1 = [SAMPLE[0]] + SAMPLE[len(SAMPLE) - i - 1:]
             result = capture(test_string)
@@ -61,7 +75,8 @@ class TestCommand(TestCase):
     def test_bad_both(self):
         for i in range(2, 6):
             for j in range(2, 6):
-                test_string = f"| readFile example_002.csv type=csv storage=pp_storage | tail {i} limit={j}"
+                test_string = f"| readFile example_002.csv type=csv " \
+                              f"storage=pp_storage | tail {i} limit={j}"
                 sample = [SAMPLE[0]] + SAMPLE[len(SAMPLE) - i + 1:]
                 sample1 = [SAMPLE[0]] + SAMPLE[len(SAMPLE) - i - 1:]
                 result = capture(test_string)
