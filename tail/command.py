d613fdfe1cc02b301c3be4cde01c4ef0bafc02ae
Nikita Serditov <nserditov@isgneuro.com>
2023-10-10 11:12:04 +0300

    now command raises valueerror when parameters number and limit are not valid

--- tail/command.py
+++ tail/command.py
@@ -1,4 +1,6 @@
-"""Module for getting a tail of a DataFrame"""
+"""Module for getting a tail of a DataFrame
+Logic is described at the OTL specifications: http://docs.isgneuro.com/1.2.0.beta/OTL/OTL_Commands/OTL_commands/#tail"""
+
 import pandas as pd
 from otlang.sdk.syntax import Keyword, Positional, OTLType
 from pp_exec_env.base_command import BaseCommand, Syntax
@@ -23,12 +25,10 @@ class TailCommand(BaseCommand):
             """
             "tail -1" or "tail 0" return no dataframe and error
             """
-            print(f"number parameter must have a positive [>=0] integer value, not {number}")
-            return pd.DataFrame()
+            raise ValueError(f"number parameter must have a positive [>=0] integer value, not {number}")
         limit = self.get_arg("limit").value
         if not_none(limit) and limit <= 0:
-            print(f"limit parameter must have a positive [>=0] integer value, not {number}")
-            return pd.DataFrame()
+            raise ValueError(f"limit parameter must have a positive [>=0] integer value, not {number}")
 
         elements: int = number if not_none(number) else (limit if not_none(limit) else DEFAULT_NUMBER)
         print(f'{number=} | {limit=} | {elements=}')
